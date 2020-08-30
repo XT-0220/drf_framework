94153efdce947c0a1faa3bd0daf8cd66c20fc054
J_XT <1606778306@qq.com>
2020-08-30 16:35:29 +0800

    改写GenericAPIView

--- drf_test/booktest/views.py
+++ drf_test/booktest/views.py
@@ -45,13 +45,19 @@ from booktest.models import BookInfo
 from booktest.serializers import BookInfoSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.generics import GenericAPIView
 class BookInfoViewSet(ModelViewSet):
     """视图集"""
     queryset = BookInfo.objects.all()
     serializer_class = BookInfoSerializer
 
 # /books/
-class BookListView(APIView):
+class BookListView(GenericAPIView):
+    # 指定序列化器类
+    serializer_class = BookInfoSerializer
+    # 指定视图所使用的查询集
+    queryset = BookInfo.objects.all()
+
     def get(self, request):
         """
         获取所有图书数据:
@@ -59,7 +65,7 @@ class BookListView(APIView):
         ② 将所有图书数据通过json进行返回
         """
         # ① 查询数据库获取所有图书数据
-        books = BookInfo.objects.all()
+        books = self.get_queryset()
 
         # ② 将所有图书数据通过json进行返回
         # books_li = []
@@ -73,7 +79,7 @@ class BookListView(APIView):
         #         'bcomment': book.bcomment
         #     }
         #     books_li.append(book_dict)
-        serializer = BookInfoSerializer(books,many=True)
+        serializer = self.get_serializer(books,many=True)
         serializer.save()
 
         return Response(serializer.data)
@@ -88,7 +94,7 @@ class BookListView(APIView):
         # ① 获取参数并进行校验
         # req_dict = json.loads(request.body.decode())   === request.data
 
-        serializer = BookInfoSerializer(data=request.data)
+        serializer = self.get_serializer(data=request.data)
 
         serializer.is_valid(raise_exception=True)
 
@@ -116,7 +122,13 @@ class BookListView(APIView):
 
 
 # /books/(?P<pk>\d+)/
-class BookDetailView(View):
+class BookDetailView(GenericAPIView):
+
+    # 指定序列化器类
+    serializer_class = BookInfoSerializer
+    # 指定视图所使用的查询集
+    queryset = BookInfo.objects.all()
+
     def get(self, request, pk):
         """
         获取指定图书数据(根据pk):
@@ -124,12 +136,8 @@ class BookDetailView(View):
         ② 将指定图书数据通过json进行返回
         """
         # ① 查询数据库获取指定的图书数据
-        try:
-            book = BookInfo.objects.get(pk=pk)
-        except BookInfo.DoesNotExist:
-            # 图书不存在
-            # return JsonResponse({'detail': 'not found'}, status=404)
-            raise Http404
+
+        book = self.get_object()
         # ② 将指定图书数据通过json进行返回
         # book_dict = {
         #     'id': book.id,
@@ -152,16 +160,12 @@ class BookDetailView(View):
         ④ 将修改图书数据通过json进行返回
         """
         # ① 查询数据库获取指定的图书数据
-        try:
-            book = BookInfo.objects.get(pk=pk)
-        except BookInfo.DoesNotExist:
-            # 图书不存在
-            raise Http404
-        # ② 获取参数并进行校验
-        req_dict = json.loads(request.body.decode())
+        book = self.get_object()
 
-        serializer = BookInfoSerializer(book,data=req_dict)
+        serializer = BookInfoSerializer(book,data=request.data)
+
         serializer.is_valid(raise_exception=True)
+
         serializer.save()
         # btitle = req_dict.get('btitle')
         # bpub_date = req_dict.get('bpub_date')
@@ -192,11 +196,7 @@ class BookDetailView(View):
         ③ 返回响应
         """
         # ① 查询数据库获取指定的图书数据
-        try:
-            book = BookInfo.objects.get(pk=pk)
-        except BookInfo.DoesNotExist:
-            # 图书不存在
-            raise Http404
+        book = self.get_object()
         # ② 删除指定图书数据
         book.delete()
 
