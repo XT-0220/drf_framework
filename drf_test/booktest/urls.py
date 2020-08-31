e3fb017a57120e02a67f486433aee2b556900565
J_XT <1606778306@qq.com>
2020-08-31 19:44:47 +0800

    视图集viewset

--- drf_test/booktest/urls.py
+++ drf_test/booktest/urls.py
@@ -1,23 +1,26 @@
 from django.urls import re_path
 from booktest import views
 
-# urls.py
-# urlpatterns = [
-#     re_path(r'^books/$', views.BookInfoViewSet.as_view()),
-#     re_path(r'^books/(?P<pk>\d+)/$', views.BookInfoViewSet.as_view()),
-# ]
-
-
-from django.urls import re_path
-from booktest import views
-
 urlpatterns = [
+    re_path(r'^books/$', views.BoonInfoViewSet.as_view({
+        "get":'list',
+    })),
+    re_path(r'^books/(?P<pk>\d+)/$', views.BoonInfoViewSet.as_view({
+        "get":'retrive'
+    })),
 ]
 
-# 路由Router
-from rest_framework.routers import DefaultRouter
-router = DefaultRouter()
-router.register('books', views.BookInfoViewSet, basename='books')
-urlpatterns += router.urls
+
+# from django.urls import re_path
+# from booktest import views
+#
+# urlpatterns = [
+# ]
+#
+# # 路由Router
+# from rest_framework.routers import DefaultRouter
+# router = DefaultRouter()
+# router.register('books', views.BookInfoViewSet, basename='books')
+# urlpatterns += router.urls
 
 
--- drf_test/booktest/views.py
+++ drf_test/booktest/views.py
@@ -1,33 +1,22 @@
-import json
-from django.http import JsonResponse, HttpResponse, Http404
-from django.views import View
-from rest_framework.views import APIView
-from rest_framework.viewsets import ModelViewSet
+from django.http import Http404
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
 from booktest.models import BookInfo
 from booktest.serializers import BookInfoSerializer
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.generics import GenericAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.mixins import ListModelMixin,CreateModelMixin,RetrieveModelMixin,DestroyModelMixin,UpdateModelMixin
-
-class BookInfoViewSet(ModelViewSet):
-    """视图集"""
-    queryset = BookInfo.objects.all()
-    serializer_class = BookInfoSerializer
 
 
-# /books/
-class BookListView(ListCreateAPIView):
-    # 指定序列化器类
-    serializer_class = BookInfoSerializer
-    # 指定视图所使用的查询集
-    queryset = BookInfo.objects.all()
+class BoonInfoViewSet(ViewSet):
+
+    def list(self,request):
+        queryset = BookInfo.objects.all()
+        serializer = BookInfoSerializer(queryset, many=True)
+        return Response(serializer.data)
 
 
-# /books/(?P<pk>\d+)/
-class BookDetailView(RetrieveUpdateDestroyAPIView):
-
-    # 指定序列化器类
-    serializer_class = BookInfoSerializer
-    # 指定视图所使用的查询集
-    queryset = BookInfo.objects.all()
+    def retrive(self,request,pk):
+        try:
+            queryset = BookInfo.objects.all()
+        except BookInfo.DoesNotExist:
+            raise Http404
+        serializer = BookInfoSerializer(queryset,many=True)
+        return Response(serializer.data)
