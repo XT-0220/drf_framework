a4786da57aba255823485c0b03d93dd45df34a26
J_XT <1606778306@qq.com>
2020-08-31 19:52:51 +0800

    视图集GenericViewSet和ListModelViewSet , RetriveModelViewSet 一起使用

--- drf_test/booktest/views.py
+++ drf_test/booktest/views.py
@@ -1,22 +1,27 @@
 from django.http import Http404
-from rest_framework.viewsets import ViewSet
+from rest_framework import mixins
+from rest_framework.viewsets import ViewSet, GenericViewSet
 from rest_framework.response import Response
 from booktest.models import BookInfo
 from booktest.serializers import BookInfoSerializer
 
 
-class BoonInfoViewSet(ViewSet):
+class BoonInfoViewSet(mixins.ListModelMixin,
+                      mixins.RetrieveModelMixin,
+                      GenericViewSet):
+    serializer_class = BookInfoSerializer
+    queryset = BookInfo.objects.all()
 
-    def list(self,request):
-        queryset = BookInfo.objects.all()
-        serializer = BookInfoSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-
-    def retrive(self,request,pk):
-        try:
-            queryset = BookInfo.objects.all()
-        except BookInfo.DoesNotExist:
-            raise Http404
-        serializer = BookInfoSerializer(queryset,many=True)
-        return Response(serializer.data)
+    # def list(self,request):
+    #     book = self.get_queryset()
+    #     serializer = BookInfoSerializer(book, many=True)
+    #     return Response(serializer.data)
+    #
+    #
+    # def retrive(self,request,pk):
+    #     try:
+    #         book = self.get_object()
+    #     except BookInfo.DoesNotExist:
+    #         raise Http404
+    #     serializer = BookInfoSerializer(book)
+    #     return Response(serializer.data)
