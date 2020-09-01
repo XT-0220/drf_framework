34722a69a32716874290f936cf582c4a87898517
J_XT <1606778306@qq.com>
2020-09-01 19:52:50 +0800

    路由router和视图集中额外API配置项生成

--- drf_test/booktest/urls.py
+++ drf_test/booktest/urls.py
@@ -1,17 +1,23 @@
-from django.urls import re_path
-from booktest import views
-
-urlpatterns = [
-    re_path(r'^books/$', views.BooKInfoViewSet.as_view({
-        "get":'list',
-        "post":'create',
-    })),
-    re_path(r'^books/(?P<pk>\d+)/$', views.BooKInfoViewSet.as_view({
-        "get": 'retrieve',
-        "put": 'update',
-        "delete": 'destroy',
-    })),
-]
+# from django.urls import re_path
+# from booktest import views
+#
+# urlpatterns = [
+#     re_path(r'^books/$', views.BooKInfoViewSet.as_view({
+#         "get":'list',
+#         "post":'create',
+#     })),
+#     re_path(r'^books/(?P<pk>\d+)/$', views.BooKInfoViewSet.as_view({
+#         "get": 'retrieve',
+#         "put": 'update',
+#         "delete": 'destroy',
+#     })),
+#     re_path(r'^books/latest/$',views.BooKInfoViewSet.as_view({
+#         'get':'laster'
+#     })),
+#     re_path(r'^books/(?P<pk>\d+)/read/$',views.BooKInfoViewSet.as_view({
+#         'put':'read'
+#     }))
+# ]
 
 
 # from django.urls import re_path
@@ -27,3 +33,15 @@ urlpatterns = [
 # urlpatterns += router.urls
 
 
+from booktest import views
+urlpatterns = [
+]
+# 1.创建Router对象。
+from rest_framework.routers import SimpleRouter
+router = SimpleRouter()
+
+# 2.注册视图集
+router.register('books', views.BooKInfoViewSet, basename='books')
+
+# 3.添加路由数据
+urlpatterns += router.urls
--- drf_test/booktest/views.py
+++ drf_test/booktest/views.py
@@ -1,5 +1,6 @@
 from django.http import Http404
 from rest_framework import mixins
+from rest_framework.decorators import action
 from rest_framework.viewsets import ViewSet, GenericViewSet, ModelViewSet
 from rest_framework.response import Response
 from booktest.models import BookInfo
@@ -7,8 +8,11 @@ from booktest.serializers import BookInfoSerializer
 
 
 class BooKInfoViewSet(ModelViewSet):
+
+    lookup_value_regex = '\d+'
     serializer_class = BookInfoSerializer
     queryset = BookInfo.objects.all()
+
 
     # def list(self,request):
     #     book = self.get_queryset()
@@ -23,3 +27,14 @@ class BooKInfoViewSet(ModelViewSet):
     #         raise Http404
     #     serializer = BookInfoSerializer(book)
     #     return Response(serializer.data)
+
+    # GET /books/latest/ -> lastest
+    @action(methods=['get'], detail=False)
+    def lastest(self, request):
+        return Response({'message': '获取id最新的图书数据'})
+
+
+    # PUT /books/(?P<pk>\d+)/read/ -> read
+    @action(methods=['get'], detail=True)
+    def read(self, request, pk):
+        return Response({'message': '修改指定图书的阅读量'})
